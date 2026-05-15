b6d8af8a86f3a495ac78031a22fde652bad327ee
Dineo <Dineo.Makofane@ext.shaper.co.za>
2026-05-15 14:57:31 +0200

    first initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+import { Hero } from './hero/hero';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Footer, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
