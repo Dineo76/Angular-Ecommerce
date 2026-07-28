a4f30b71c5e5363e556ccc8e6779cd76da075fd3
Dineo76 <dineomakofane2@gmail.com>
2026-07-28 21:55:21 +0200

    updated orders ui page

--- src/app/orders/orders.ts
+++ src/app/orders/orders.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../service/cart.service';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './orders.html',
   styleUrls: ['./orders.css']
 })
